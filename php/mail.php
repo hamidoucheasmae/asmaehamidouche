0637aacc1a1f94e892e872233ea6b8dcb9db1ea5
Asmae Hamidouche <76485278+hamidoucheasmae@users.noreply.github.com>
2022-10-12 10:02:33 +0100

    test captcha

--- php/mail.php
+++ php/mail.php
@@ -17,4 +17,40 @@ else
 {
   echo 'failed';
 }
+
+
+
+
+session_start();
+    
+if(!empty($_POST["send"])) {
+  $Name = $_POST["name"];
+  $E-mail = $_POST["email"];
+  $Phone = $_POST["phone"];
+  $Website = $_POST["website"];
+  $Message = $_POST["message"];
+  $captcha = $_POST["captcha"];
+
+  $captchaUser = filter_var($_POST["captcha"], FILTER_SANITIZE_STRING);
+
+  if(empty($captcha)) {
+    $captchaError = array(
+      "status" => "alert-danger",
+      "message" => "Please enter the captcha."
+    );
+  }
+  else if($_SESSION['CAPTCHA_CODE'] == $captchaUser){
+    $captchaError = array(
+      "status" => "alert-success",
+      "message" => "Your form has been submitted successfuly."
+    );
+  } else {
+    $captchaError = array(
+      "status" => "alert-danger",
+      "message" => "Captcha is invalid."
+    );
+  }
+}  
+
 ?>
+
